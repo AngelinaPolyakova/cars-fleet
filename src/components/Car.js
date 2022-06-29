6d0cc7c9cbcba7fa031333fc1f140f3af9de5681
Angelina Polyakova <lina.krasilnikova.1996@mail.ru>
2022-06-29 11:14:05 +0400

    fixed flex-items and margin details

--- src/components/Car.js
+++ src/components/Car.js
@@ -17,9 +17,9 @@ function Car(props) {
         <h3 className="cars__item-model"> {props.model}</h3>
         <p className="cars__item-year"> {props.year}</p>
         <div className={`description-details ${isShow ? "hide" : ""}`}>
-          <p>{props.color}</p>
-          <p>{props.engine}</p>
-          <p>{props.transmission}</p>
+          <p>color: {props.color}</p>
+          <p>engine: {props.engine}</p>
+          <p>transmission: {props.transmission}</p>
         </div>
       </div>
       <button className="cars__item-btn btn" onClick={showDetails}>
